3a7b11d19b7721970eeffad7c2617c9e151fec84
Matthew Boynes <git@mattboynes.com>
2020-02-19 20:21:11 -0500

    Initial commit

---
+++ mantle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Mantle
+ * Plugin URI:  https://github.com/alleyinteractive/mantle
+ * Description: A framework for powerful WordPress development
+ * Author:      Alley
+ * Author URI:  https://alley.co/
+ * Text Domain: mantle
+ * Domain Path: /languages
+ * Version:     0.1
+ *
+ * @package Mantle
+ */
