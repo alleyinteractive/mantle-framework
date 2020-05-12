0b2d884e79ce91148d56abd2f6ab9e0b5a75d56a
Dustin Paluch <dustin@alley.co>
2020-05-11 21:39:34 -0400

    Implement ArrayAccess methods

--- src/mantle/framework/collection/class-collection.php
+++ src/mantle/framework/collection/class-collection.php
@@ -11,4 +11,49 @@ use ArrayAccess;
 
 class Collection implements ArrayAccess {
 
+	/**
+	 * Determine if a given offset exists.
+	 *
+	 * @param  string $key
+	 * @return bool
+	 */
+	public function offsetExists( mixed $offset ): bool {
+		return isset( $this->items[$offset] );
+	}
+
+	/**
+	 * Get the value at a given offset.
+	 *
+	 * @param  string $key
+	 * @return mixed
+	 */
+	public function offsetGet( mixed $offset ): mixed {
+		return $this->items[$offset];
+	}
+
+	/**
+	 * Set the value at a given offset.
+	 *
+	 * @param  string $key
+	 * @param  mixed  $value
+	 * @return void
+	 */
+	public function offsetSet( mixed $offset , mixed $value ): void {
+		if ( is_null( $offset ) ) {
+			$this->items[] = $value;
+		} else {
+			$this->items[$offset] = $value;
+		}
+	}
+
+	/**
+	 * Unset the value at a given offset.
+	 *
+	 * @param  string $key
+	 * @return void
+	 */
+	public function offsetUnset( mixed $offset ): void {
+		unset( $this->items[$offset] );
+	}
+
 }
