3c344ef84d8ea1fecfbe7429f6653be4b3f22a94
Dustin Paluch <dustin@alley.co>
2020-05-13 08:53:27 -0400

    Add Arrayable and Jsonable Interfaces

---
+++ src/mantle/framework/contracts/support/interface-arrayable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mantle\Framework\Contracts\Support;
+
+interface Arrayable {
+  /**
+   * Get the instance as an array.
+   *
+   * @return array
+   */
+  public function toArray();
+}
