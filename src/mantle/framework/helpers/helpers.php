65cb9c803e6bc08a5d42ad6620b13516bac821f5
Dustin Paluch <dustin@alley.co>
2020-05-14 15:22:14 -0400

    Remove collect() helper
    Someone else ported it and landed it in master already.

--- src/mantle/framework/helpers/helpers.php
+++ src/mantle/framework/helpers/helpers.php
@@ -10,4 +10,3 @@ namespace Mantle\Framework\Helpers;
 require_once __DIR__ . '/helpers-general.php';
 require_once __DIR__ . '/helpers-core-objects.php';
 require_once __DIR__ . '/helpers-array.php';
-require_once __DIR__ . '/helpers-collection.php';
