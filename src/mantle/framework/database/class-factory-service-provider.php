1ba6153f8ab44f8a93bf042dda510081a3182660
Eric Fuller <eric@alley.co>
2020-05-18 09:03:18 -0400

    Factory service provider. WIP

--- src/mantle/framework/database/class-factory-service-provider.php
+++ src/mantle/framework/database/class-factory-service-provider.php
@@ -7,6 +7,9 @@
 
 namespace Mantle\Framework\Database;
 
+use \Faker\Factory;
+use \Faker\Generator as FakerGenerator;
+use Mantle\Framework\Database\Factory\Factory as MantleFactory;
 use Mantle\Framework\Service_Provider;
 
 /**
@@ -16,9 +19,45 @@ use Mantle\Framework\Service_Provider;
  */
 class Factory_Service_Provider extends Service_Provider {
 	/**
+	 * The array of resolved Faker instances.
+	 *
+	 * @var array
+	 */
+	protected static $fakers = [];
+
+	/**
 	 * Register any application services.
 	 */
 	public function register() {
 		$this->add_command( Console\Seed_Command::class );
+
+		$this->registerEloquentFactory();
+	}
+
+	/**
+	 * Register the Eloquent factory instance in the container.
+	 *
+	 * @return void
+	 */
+	protected function registerEloquentFactory()
+	{
+//		$this->app->singleton(Generator::class, function ($app, $parameters) {
+////			$locale = $parameters['locale'] ?? $app['config']->get('app.faker_locale', 'en_US');
+//			$locale = 'en_US';
+//
+//			if (! isset(static::$fakers[$locale])) {
+//				static::$fakers[$locale] = Factory::create();
+//			}
+//
+//			static::$fakers[$locale]->unique(true);
+//
+//			return static::$fakers[$locale];
+//		});
+
+		$this->app->singleton(MantleFactory::class, function ($app) {
+			return MantleFactory::construct(
+				$app->make(FakerGenerator::class), '/Users/ericfuller/broadway/www/mantle/wp-content/private/mantle/database/factories'
+			);
+		});
 	}
 }
