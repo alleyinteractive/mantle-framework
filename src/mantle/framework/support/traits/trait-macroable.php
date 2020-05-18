cf47d8185bfee1a20a0df57b03815d73e3b42eea
Eric Fuller <eric@alley.co>
2020-05-18 15:35:46 -0400

    More PHPCS.

--- src/mantle/framework/support/traits/trait-macroable.php
+++ src/mantle/framework/support/traits/trait-macroable.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Macroable class file.
+ *
+ * @package Mantle
+ */
+
+// phpcs:disable Squiz.Commenting.FunctionComment.MissingParamComment
+// phpcs:ignoreFile: WordPressVIPMinimum.Variables.VariableAnalysis.StaticInsideClosure
 
 namespace Mantle\Framework\Support\Traits;
 
@@ -7,8 +15,7 @@ use Closure;
 use ReflectionClass;
 use ReflectionMethod;
 
-trait Macroable
-{
+trait Macroable {
 	/**
 	 * The registered string macros.
 	 *
@@ -19,34 +26,34 @@ trait Macroable
 	/**
 	 * Register a custom macro.
 	 *
-	 * @param  string  $name
-	 * @param  object|callable  $macro
+	 * @param string          $name
+	 * @param object|callable $macro
+	 *
 	 * @return void
 	 */
-	public static function macro($name, $macro)
-	{
-		static::$macros[$name] = $macro;
+	public static function macro( $name, $macro ) {
+		static::$macros[ $name ] = $macro;
 	}
 
 	/**
 	 * Mix another object into the class.
 	 *
-	 * @param  object  $mixin
-	 * @param  bool  $replace
+	 * @param object $mixin
+	 * @param bool   $replace
+	 *
 	 * @return void
 	 *
 	 * @throws \ReflectionException
 	 */
-	public static function mixin($mixin, $replace = true)
-	{
-		$methods = (new ReflectionClass($mixin))->getMethods(
+	public static function mixin( $mixin, $replace = true ) {
+		$methods = ( new ReflectionClass( $mixin ) )->getMethods(
 			ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED
 		);
 
-		foreach ($methods as $method) {
-			if ($replace || ! static::hasMacro($method->name)) {
-				$method->setAccessible(true);
-				static::macro($method->name, $method->invoke($mixin));
+		foreach ( $methods as $method ) {
+			if ( $replace || ! static::hasMacro( $method->name ) ) {
+				$method->setAccessible( true );
+				static::macro( $method->name, $method->invoke( $mixin ) );
 			}
 		}
 	}
@@ -54,63 +61,63 @@ trait Macroable
 	/**
 	 * Checks if macro is registered.
 	 *
-	 * @param  string  $name
+	 * @param string $name
+	 *
 	 * @return bool
 	 */
-	public static function hasMacro($name)
-	{
-		return isset(static::$macros[$name]);
+	public static function hasMacro( $name ) {
+		return isset( static::$macros[ $name ] );
 	}
 
 	/**
 	 * Dynamically handle calls to the class.
 	 *
-	 * @param  string  $method
-	 * @param  array  $parameters
+	 * @param string $method
+	 * @param array  $parameters
+	 *
 	 * @return mixed
 	 *
 	 * @throws \BadMethodCallException
 	 */
-	public static function __callStatic($method, $parameters)
-	{
-		if (! static::hasMacro($method)) {
-			throw new BadMethodCallException(sprintf(
+	public static function __callStatic( $method, $parameters ) {
+		if ( ! static::hasMacro( $method ) ) {
+			throw new BadMethodCallException( sprintf(
 				'Method %s::%s does not exist.', static::class, $method
-			));
+			) );
 		}
 
-		$macro = static::$macros[$method];
+		$macro = static::$macros[ $method ];
 
-		if ($macro instanceof Closure) {
-			return call_user_func_array(Closure::bind($macro, null, static::class), $parameters);
+		if ( $macro instanceof Closure ) {
+			return call_user_func_array( Closure::bind( $macro, null, static::class ), $parameters );
 		}
 
-		return $macro(...$parameters);
+		return $macro( ...$parameters );
 	}
 
 	/**
 	 * Dynamically handle calls to the class.
 	 *
-	 * @param  string  $method
-	 * @param  array  $parameters
+	 * @param string $method
+	 * @param array  $parameters
+	 *
 	 * @return mixed
 	 *
 	 * @throws \BadMethodCallException
 	 */
-	public function __call($method, $parameters)
-	{
-		if (! static::hasMacro($method)) {
-			throw new BadMethodCallException(sprintf(
+	public function __call( $method, $parameters ) {
+		if ( ! static::hasMacro( $method ) ) {
+			throw new BadMethodCallException( sprintf(
 				'Method %s::%s does not exist.', static::class, $method
-			));
+			) );
 		}
 
-		$macro = static::$macros[$method];
+		$macro = static::$macros[ $method ];
 
-		if ($macro instanceof Closure) {
-			return call_user_func_array($macro->bindTo($this, static::class), $parameters);
+		if ( $macro instanceof Closure ) {
+			return call_user_func_array( $macro->bindTo( $this, static::class ), $parameters );
 		}
 
-		return $macro(...$parameters);
+		return $macro( ...$parameters );
 	}
 }
