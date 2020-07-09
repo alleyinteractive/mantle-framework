11b7e00d29bcb5d1ba0c97b3e29b31fae67fff0f
Sean Fisher <srtfisher@gmail.com>
2020-07-09 17:55:09 -0400

    Adding the application level app service provider

---
+++ src/mantle/framework/providers/class-app-service-provider.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * App_Service_Provider class file.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Framework\Providers;
+
+use Mantle\Framework\Contracts\Application;
+use Mantle\Framework\Service_Provider;
+
+use function Mantle\Framework\Helpers\tap;
+
+/**
+ * App Service Provider
+ */
+class App_Service_Provider extends Service_Provider {
+	/**
+	 * Application instance.
+	 *
+	 * @var Application
+	 */
+	protected $app;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Application $app Application instance.
+	 */
+	public function __construct( Application $app ) {
+		$this->app = $app;
+
+		$this->app->booted(
+			function() {
+				$this->boot_scheduler();
+			}
+		);
+	}
+
+	/**
+	 * Boot the scheduler service.
+	 */
+	protected function boot_scheduler() {
+		$this->app->singleton(
+			Schedule::class,
+			function( $app ) {
+				return tap(
+					new Schedule( $app ),
+					function( $schedule) {
+						$this->schedule( $schedule );
+					}
+				);
+			}
+		);
+	}
+
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param Schedule $schedule Schedule instance.
+	 */
+	protected function schedule( Schedule $schedule ) {
+		//
+	}
+}
