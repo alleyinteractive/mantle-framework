200550dd8937a7623cdac07bbfdd6fd0d690ca23
Sean Fisher <srtfisher@gmail.com>
2020-07-09 16:44:03 -0400

    Remove the check on the model

--- tests/support/providers/test-model-register-provider.php
+++ tests/support/providers/test-model-register-provider.php
@@ -40,30 +40,4 @@ class Test_Model_Register_Provider extends MockeryTestCase {
 		// Call 'init' to allow the register_object method to be called.
 		do_action( 'init' );
 	}
-
-	public function test_register_invalid_model() {
-		$app    = new Application();
-		$config = new Repository();
-
-		$this->expectException( Provider_Exception::class );
-		$this->expectExceptionMessage(
-			Invalid_Model_Example::class . ' does not implement ' . Registrable_Contract::class . ' interface'
-		);
-
-		$app->instance( 'config', $config );
-		$config->set(
-			'models',
-			[
-				'register' => [
-					Invalid_Model_Example::class,
-				],
-			]
-		);
-
-		$provider = new Model_Register_Provider( $app );
-		$provider->register();
-		$provider->boot();
-	}
 }
-
-class Invalid_Model_Example { }
