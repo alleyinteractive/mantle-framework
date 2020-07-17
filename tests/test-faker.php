44d369bc0ffb3b4bb8c90f09660ba908ddc8d316
Sean Fisher <srtfisher@gmail.com>
2020-07-17 11:51:36 -0400

    Add test for faker

---
+++ tests/test-faker.php
@@ -0,0 +1,48 @@
+<?php
+namespace Mantle\Tests;
+
+use Faker\Factory;
+use Mantle\Framework\Faker\Faker_Provider;
+
+class Test_Faker extends \PHPUnit\Framework\TestCase {
+	/**
+	 * @var Factory
+	 */
+	protected $faker;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->faker = Factory::create();
+		$this->faker->addProvider( new Faker_Provider( $this->faker ) );
+	}
+
+	public function test_block() {
+		$this->assertEquals(
+			'<!-- wp:namespace/block /-->',
+			$this->faker->block( 'namespace/block' )
+		);
+
+		$this->assertEquals(
+			'<!-- wp:namespace/block {"exampleAttr":true,"another":false} /-->',
+			$this->faker->block( 'namespace/block', '', [ 'exampleAttr' => true, 'another' => false ] )
+		);
+
+		$this->assertEquals(
+			sprintf( '<!-- wp:namespace/block {"exampleAttr":true,"another":false} -->%1$sThe Content%1$s<!-- /wp:namespace/block -->', PHP_EOL ),
+			$this->faker->block( 'namespace/block', 'The Content', [ 'exampleAttr' => true, 'another' => false ] )
+		);
+
+		$this->assertEquals(
+			sprintf( '<!-- wp:namespace/block -->%1$sThe Content%1$s<!-- /wp:namespace/block -->', PHP_EOL ),
+			$this->faker->block( 'namespace/block', 'The Content', [] )
+		);
+	}
+
+	public function test_paragraph() {
+		$block = $this->faker->paragraph_block();
+
+		$this->assertContains( '<!-- wp:paragraph -->' . PHP_EOL . '<p>', $block );
+		$this->assertContains( '</p>' . PHP_EOL . '<!-- /wp:paragraph -->', $block );
+	}
+}
