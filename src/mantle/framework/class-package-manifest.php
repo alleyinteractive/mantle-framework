5120a92986e4a4bcbfa150f34d65986db2105a32
Sean Fisher <srtfisher@gmail.com>
2020-08-14 11:36:31 -0400

    Update src/mantle/framework/class-package-manifest.php
    Co-authored-by: Kevin Fodness <kevin@kevinfodness.com>

--- src/mantle/framework/class-package-manifest.php
+++ src/mantle/framework/class-package-manifest.php
@@ -47,9 +47,9 @@ class Package_Manifest {
 	/**
 	 * Constructor.
 	 *
-	 * @param string      $base_path Base folder path for the Mantle site.
+	 * @param string      $base_path     Base folder path for the Mantle site.
 	 * @param string      $manifest_path Path to the package manifest file.
-	 * @param Application $app Application instance.
+	 * @param Application $app           Application instance.
 	 */
 	public function __construct( string $base_path, string $manifest_path, Application $app ) {
 		$this->base_path     = $base_path;
