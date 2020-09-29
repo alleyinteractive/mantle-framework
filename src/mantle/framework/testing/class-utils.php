25702e4eaebd8e5418ec45a0ac17effb1a3577e9
Sean Fisher <srtfisher@gmail.com>
2020-09-29 14:47:43 -0400

    Fixing indent

--- src/mantle/framework/testing/class-utils.php
+++ src/mantle/framework/testing/class-utils.php
@@ -171,14 +171,14 @@ class Utils {
 		defined( 'DB_CHARSET' ) || define( 'DB_CHARSET', 'utf8' );
 		defined( 'DB_COLLATE' ) || define( 'DB_COLLATE', '' );
 
-		defined( 'AUTH_KEY' ) || define( 'AUTH_KEY',         'put your unique phrase here' );
-		defined( 'SECURE_AUTH_KEY' ) || define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-		defined( 'LOGGED_IN_KEY' ) || define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-		defined( 'NONCE_KEY' ) || define( 'NONCE_KEY',        'put your unique phrase here' );
-		defined( 'AUTH_SALT' ) || define( 'AUTH_SALT',        'put your unique phrase here' );
+		defined( 'AUTH_KEY' ) || define( 'AUTH_KEY', 'put your unique phrase here' );
+		defined( 'SECURE_AUTH_KEY' ) || define( 'SECURE_AUTH_KEY', 'put your unique phrase here' );
+		defined( 'LOGGED_IN_KEY' ) || define( 'LOGGED_IN_KEY', 'put your unique phrase here' );
+		defined( 'NONCE_KEY' ) || define( 'NONCE_KEY', 'put your unique phrase here' );
+		defined( 'AUTH_SALT' ) || define( 'AUTH_SALT', 'put your unique phrase here' );
 		defined( 'SECURE_AUTH_SALT' ) || define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-		defined( 'LOGGED_IN_SALT' ) || define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-		defined( 'NONCE_SALT' ) || define( 'NONCE_SALT',       'put your unique phrase here' );
+		defined( 'LOGGED_IN_SALT' ) || define( 'LOGGED_IN_SALT', 'put your unique phrase here' );
+		defined( 'NONCE_SALT' ) || define( 'NONCE_SALT', 'put your unique phrase here' );
 
 		$table_prefix = 'wptests_';
 
