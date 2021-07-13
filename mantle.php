5784a321a97e1320af63ec886123a9c8ca87be2e
Sean Fisher <srtfisher@gmail.com>
2021-07-12 21:39:24 -0400

    Remove plugin header from framework

--- mantle.php
+++ mantle.php
@@ -1,16 +1,13 @@
 <?php
 /**
- * Plugin Name: Mantle
- * Plugin URI:  https://github.com/alleyinteractive/mantle
- * Description: A framework for powerful WordPress development
- * Author:      Alley
- * Author URI:  https://alley.co/
- * Text Domain: mantle
- * Domain Path: /languages
- * Version:     0.1
+ * Mantle Framework
+ *
+ * A framework for powerful WordPress development.
  *
  * Mantle is a derivative work of Laravel by Taylor Otwell (licensed MIT).
  *
+ * @link https://github.com/alleyinteractive/mantle-framework
+ * @author Alley <hello@alley.co>
  * @package Mantle
  */
 
