1e47f7ce208e0c966c1f50911bd7517d69f3e373
Sean Fisher <srtfisher@gmail.com>
2022-03-25 15:01:37 -0400

    Fixing mono repo

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,8 +1,11 @@
 <?php
 /**
  * Monorepo Builder
- *
  * For internal use only.
+ *
+ * phpcs:disable
+ *
+ * @package Mantle
  */
 
 declare(strict_types=1);
@@ -34,9 +37,9 @@ return static function ( ContainerConfigurator $container_config ): void {
 	$parameters->set(
 		Option::DATA_TO_APPEND,
 		[
-			ComposerJsonSection::REQUIRE => [
+			ComposerJsonSection::REQUIRE     => [
 				'alleyinteractive/composer-wordpress-autoloader' => '^0.2',
-				'php'                                            => '^7.4|^8.0',
+				'php' => '^7.4|^8.0',
 			],
 			ComposerJsonSection::REQUIRE_DEV => [
 				'alleyinteractive/alley-coding-standards' => '^0.3',
@@ -44,35 +47,4 @@ return static function ( ContainerConfigurator $container_config ): void {
 			],
 		],
 	);
-
-	// Remove the mantle packages from the composer.json (temporary).
-	// $parameters->set(
-	// 	Option::DATA_TO_REMOVE,
-	// 	[
-	// 		ComposerJsonSection::REQUIRE => [
-	// 			'mantle-framework/assets' => '*',
-	// 			'mantle-framework/auth' => '*',
-	// 			'mantle-framework/cache' => '*',
-	// 			'mantle-framework/caper' => '*',
-	// 			'mantle-framework/config' => '*',
-	// 			'mantle-framework/console' => '*',
-	// 			'mantle-framework/container' => '*',
-	// 			'mantle-framework/contracts' => '*',
-	// 			'mantle-framework/database' => '*',
-	// 			'mantle-framework/events' => '*',
-	// 			'mantle-framework/facade' => '*',
-	// 			'mantle-framework/faker' => '*',
-	// 			'mantle-framework/filesystem' => '*',
-	// 			'mantle-framework/http' => '*',
-	// 			'mantle-framework/log' => '*',
-	// 			'mantle-framework/query-monitor' => '*',
-	// 			'mantle-framework/queue' => '*',
-	// 			'mantle-framework/rest-api' => '*',
-	// 			'mantle-framework/scheduling' => '*',
-	// 			'mantle-framework/support' => '*',
-	// 			'mantle-framework/testing' => '*',
-	// 			'mantle-framework/view' => '*',
-	// 		],
-	// 	],
-	// );
 };
