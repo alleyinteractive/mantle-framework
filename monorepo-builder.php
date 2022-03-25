b50a67b2100771c67ce1378b670b2116bded2272
Sean Fisher <srtfisher@gmail.com>
2022-03-25 13:28:13 -0400

    Wiring up builder

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Monorepo Builder
+ *
+ * For internal use only.
+ */
 
 declare(strict_types=1);
 
@@ -25,26 +30,53 @@ return static function ( ContainerConfigurator $container_config ): void {
 		],
 	);
 
-	// Override the 'extra' section of the composer.json.
-	// $parameters->set(
-	// 	Option::DATA_TO_APPEND,
-	// 	[
-	// 		ComposerJsonSection::EXTRA,
-	// 	],
-	// );
-
-	// for "merge" command
+	// Append specific items to the composer.json.
 	$parameters->set(
 		Option::DATA_TO_APPEND,
 		[
 			ComposerJsonSection::REQUIRE => [
 				'alleyinteractive/composer-wordpress-autoloader' => '^0.2',
-				'php' => '^7.4|^8.0',
+				'php'                                            => '^7.4|^8.0',
 			],
 			ComposerJsonSection::REQUIRE_DEV => [
 				'alleyinteractive/alley-coding-standards' => '^0.3',
-				'phpunit/phpunit' => '^8.5.8 || ^9.3.3',
+				'phpunit/phpunit'                         => '^8.5.8 || ^9.3.3',
 			],
+		],
+	);
+
+	// Remove the mantle packages from the composer.json (temporary).
+	$parameters->set(
+		Option::DATA_TO_REMOVE,
+		[
+			ComposerJsonSection::REQUIRE => [
+				'mantle-framework/assets' => '*',
+				'mantle-framework/auth' => '*',
+				'mantle-framework/cache' => '*',
+				'mantle-framework/caper' => '*',
+				'mantle-framework/config' => '*',
+				'mantle-framework/console' => '*',
+				'mantle-framework/container' => '*',
+				'mantle-framework/contracts' => '*',
+				'mantle-framework/database' => '*',
+				'mantle-framework/events' => '*',
+				'mantle-framework/facade' => '*',
+				'mantle-framework/faker' => '*',
+				'mantle-framework/filesystem' => '*',
+				'mantle-framework/http' => '*',
+				'mantle-framework/log' => '*',
+				'mantle-framework/query-monitor' => '*',
+				'mantle-framework/queue' => '*',
+				'mantle-framework/rest-api' => '*',
+				'mantle-framework/scheduling' => '*',
+				'mantle-framework/support' => '*',
+				'mantle-framework/testing' => '*',
+				'mantle-framework/view' => '*',
+
+				// todo: fix the location.
+				'mantle-framework/framework' => '*',
+				'mantle-framework/helpers' => '*',
+			]
 		],
 	);
 };
