fdc92f18f6f4c6bbc1cbed8505c4a7351c7410ca
Sean Fisher <srtfisher@gmail.com>
2022-04-08 11:38:17 -0400

    Adding command to setup repos in github

---
+++ bin/reprepository-create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create the mantle-framework repositories for subtree splitting.
+# Requires `gh` and `git` to be installed.
+
+
+PACKAGES=$(find src/mantle -type d -exec test -e '{}'/composer.json \;  -print)
+ORGANIZATION="mantle-framework"
+
+if ! type gh &> /dev/null; then
+	echo "gh is not installed. Please install it: brew install gh"
+	exit 1
+fi
+
+for path in $PACKAGES; do
+	package=$(basename $path)
+	echo "Checking $ORGANIZATION/$package..."
+
+	# Fetch the package from GitHub.
+	gh repo view "$ORGANIZATION/$package" > /dev/null 2>&1
+
+	if [ $? -ne 0 ]; then
+		echo "Repository not found. Creating..."
+		gh repo create "$ORGANIZATION/$package" -d "[READ ONLY] Subtree split of the Mantle $package package" --public
+	fi
+done
+
+echo "DONE"
