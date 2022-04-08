5d2afb8bda523155b7256a5f9cd19335789f0f6d
Sean Fisher <srtfisher@gmail.com>
2022-04-08 14:13:40 -0400

    Adding check for empty repo

--- bin/repository-create.sh
+++ bin/repository-create.sh
@@ -11,6 +11,13 @@ if ! type gh &> /dev/null; then
 	exit 1
 fi
 
+if ! type jq &> /dev/null; then
+	echo "jq is not installed. Please install it: brew install jq"
+	exit 1
+fi
+
+cwd=$(pwd)
+
 for path in $PACKAGES; do
 	package=$(basename "$path")
 	echo "Checking $ORGANIZATION/$package..."
@@ -19,6 +26,27 @@ for path in $PACKAGES; do
 	if ! gh repo view "$ORGANIZATION/$package" > /dev/null 2>&1; then
 		echo "Repository not found. Creating..."
 		gh repo create "$ORGANIZATION/$package" -d "[READ ONLY] Subtree split of the Mantle $package package" --public
+	fi
+
+	is_empty=$(gh repo view mantle-framework/auth --json=isEmpty | jq -r .isEmpty)
+
+	# Initialize the repository if it is empty.
+	if [ "$is_empty" = "true" ]; then
+		echo "Creating default branch..."
+		clone_dir="/tmp/tmp-$package"
+
+		# Clear the existing directory and clone the repository.
+		rm -rf "$clone_dir"
+
+		gh repo clone "$ORGANIZATION/$package" "$clone_dir"
+
+		cd "$clone_dir" || exit
+
+		touch .gitignore
+		git add --all . && git commit -m "Initializing repository"
+		git push -u origin main
+
+		cd "$cwd" || exit
 	fi
 
 	# Update the settings to disable issues and wikis.
