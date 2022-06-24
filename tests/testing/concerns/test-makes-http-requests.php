304f0e053476c8e055479ec2ec7b03ac35873998
Sean Fisher <srtfisher@gmail.com>
2022-06-24 15:42:36 -0400

    phpcs fixes

--- tests/testing/concerns/test-makes-http-requests.php
+++ tests/testing/concerns/test-makes-http-requests.php
@@ -175,31 +175,72 @@ class Test_Makes_Http_Requests extends Framework_Test_Case {
 class JsonSerializableMixedResourcesStub implements JsonSerializable {
 	public function jsonSerialize(): array {
 		return [
-			'foo' => 'bar',
-			'foobar' => [
+			'foo'          => 'bar',
+			'foobar'       => [
 				'foobar_foo' => 'foo',
 				'foobar_bar' => 'bar',
 			],
-			'0' => ['foo'],
-			'bars' => [
-				['bar' => 'foo 0', 'foo' => 'bar 0'],
-				['bar' => 'foo 1', 'foo' => 'bar 1'],
-				['bar' => 'foo 2', 'foo' => 'bar 2'],
+			'0'            => [ 'foo' ],
+			'bars'         => [
+				[
+					'bar' => 'foo 0',
+					'foo' => 'bar 0',
+				],
+				[
+					'bar' => 'foo 1',
+					'foo' => 'bar 1',
+				],
+				[
+					'bar' => 'foo 2',
+					'foo' => 'bar 2',
+				],
 			],
-			'baz' => [
-				['foo' => 'bar 0', 'bar' => ['foo' => 'bar 0', 'bar' => 'foo 0']],
-				['foo' => 'bar 1', 'bar' => ['foo' => 'bar 1', 'bar' => 'foo 1']],
+			'baz'          => [
+				[
+					'foo' => 'bar 0',
+					'bar' => [
+						'foo' => 'bar 0',
+						'bar' => 'foo 0',
+					],
+				],
+				[
+					'foo' => 'bar 1',
+					'bar' => [
+						'foo' => 'bar 1',
+						'bar' => 'foo 1',
+					],
+				],
 			],
-			'barfoo' => [
-				['bar' => ['bar' => 'foo 0']],
-				['bar' => ['bar' => 'foo 0', 'foo' => 'foo 0']],
-				['bar' => ['foo' => 'bar 0', 'bar' => 'foo 0', 'rab' => 'rab 0']],
+			'barfoo'       => [
+				[ 'bar' => [ 'bar' => 'foo 0' ] ],
+				[
+					'bar' => [
+						'bar' => 'foo 0',
+						'foo' => 'foo 0',
+					],
+				],
+				[
+					'bar' => [
+						'foo' => 'bar 0',
+						'bar' => 'foo 0',
+						'rab' => 'rab 0',
+					],
+				],
 			],
 			'numeric_keys' => [
-				2 => ['bar' => 'foo 0', 'foo' => 'bar 0'],
-				3 => ['bar' => 'foo 1', 'foo' => 'bar 1'],
-				4 => ['bar' => 'foo 2', 'foo' => 'bar 2'],
+				2 => [
+					'bar' => 'foo 0',
+					'foo' => 'bar 0',
+				],
+				3 => [
+					'bar' => 'foo 1',
+					'foo' => 'bar 1',
+				],
+				4 => [
+					'bar' => 'foo 2',
+					'foo' => 'bar 2',
+				],
 			],
 		];
-  }
+	}
 }
