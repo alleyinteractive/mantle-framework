66b536d804959f86cccee07adfd99506ca24a176
Sean Fisher <srtfisher@gmail.com>
2022-07-27 19:22:51 -0400

    Adding an Installation_Manager

---
+++ src/mantle/testing/class-installation-manager.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Installation_Manager class file
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Testing;
+
+use Mantle\Support\Traits\Singleton;
+
+/**
+ * Installation Manager
+ */
+class Installation_Manager {
+	use Singleton;
+
+	/**
+	 * Callbacks for before installation.
+	 *
+	 * @var callable[]
+	 */
+	protected array $before_install_callbacks = [];
+
+	/**
+	 * Callbacks for after installation.
+	 *
+	 * @var callable[]
+	 */
+	protected array $after_install_callbacks = [];
+
+	/**
+	 * Callback for after WordPress is loaded.
+	 *
+	 * @var callable[]
+	 */
+	protected array $after_loaded_callbacks = [];
+
+	/**
+	 * Define a callback to be invoked before installation.
+	 *
+	 * @param callable $callback Callback to invoke before installation.
+	 * @return static
+	 */
+	public function before( ?callable $callback ) {
+		if ( is_callable( $callback ) ) {
+			$this->before_install_callbacks[] = $callback;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Define a callback to be invoked after installation.
+	 *
+	 * @param callable $callback Callback to invoke after installation.
+	 * @return static
+	 */
+	public function after( ?callable $callback ) {
+		if ( is_callable( $callback ) ) {
+			$this->after_install_callbacks[] = $callback;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Define a callback to be invoked after WordPress is loaded.
+	 *
+	 * @param callable $callback Callback to invoke after WordPress is loaded.
+	 * @return static
+	 */
+	public function loaded( ?callable $callback ) {
+		return $this->on( 'wp_loaded', $callback );
+	}
+
+	/**
+	 * Define a callback for a specific WordPress hook.
+	 *
+	 * @param string   $hook Hook name.
+	 * @param callable $callback Callback to invoke.
+	 * @param int      $priority Priority.
+	 * @return static
+	 */
+	public function on( string $hook, ?callable $callback, int $priority = 10 ) {
+		if ( is_callable( $callback ) ) {
+			tests_add_filter( $hook, $callback, $priority );
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Install the Mantle Testing Framework.
+	 *
+	 * @return static
+	 */
+	public function install() {
+		foreach ( $this->before_install_callbacks as $callback ) {
+			$callback();
+		}
+
+		try {
+			require_once __DIR__ . '/wordpress-bootstrap.php';
+		} catch ( \Throwable $throwable ) {
+			echo "ERROR: Failed to load WordPress!\n";
+			echo "{$throwable}\n"; // phpcs:ignore
+			exit( 1 );
+		}
+
+		foreach ( $this->after_install_callbacks as $callback ) {
+			$callback();
+		}
+
+		return $this;
+	}
+}
--- src/mantle/testkit/concerns/trait-installs-wordpress.php
+++ src/mantle/testkit/concerns/trait-installs-wordpress.php
@@ -14,9 +14,10 @@ trait Installs_WordPress {
 	/**
 	 * Manages installing WordPress.
 	 *
-	 * This is useful for installing WordPress in integration tests, while leaving the bootstrap
-	 * free of this overhead during unit tests. To perform actions after WordPress is installed, you
-	 * simply need to define a function `mantle_after_wordpress_install` in your bootstrap file.
+	 * This is useful for installing WordPress in integration tests, while leaving
+	 * the bootstrap free of this overhead during unit tests. To perform actions
+	 * after WordPress is installed,  you can use the Installation Manager to
+	 * define a callback to be invoked before/after installation.
 	 */
 	public static function installs_wordpress_set_up_before_class(): void {
 		static $installed = false;
@@ -25,6 +26,11 @@ trait Installs_WordPress {
 			return;
 		}
 
+		/**
+		 * Fire a callback to a named function called 'mantle_after_wordpress_install'.
+		 *
+		 * @deprecated Logic condensed into {@see \Mantle\Testing\Installation_Manager::after()}.
+		 */
 		$callback = function_exists( 'mantle_after_wordpress_install' ) ? 'mantle_after_wordpress_install' : null;
 
 		\Mantle\Testing\install( $callback );
