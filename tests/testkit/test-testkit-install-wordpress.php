9d6dd4c71684dcf68346b749f60fc8c73f1034f9
Sean Fisher <srtfisher@gmail.com>
2022-07-27 19:29:59 -0400

    Adding test back for mantle_after_wordpress_install

--- tests/testkit/test-testkit-install-wordpress.php
+++ tests/testkit/test-testkit-install-wordpress.php
@@ -5,6 +5,9 @@ use Mantle\Testing\Installation_Manager;
 use Mantle\Testkit\Concerns\Installs_WordPress;
 use Mantle\Testkit\Test_Case;
 
+// Required for callback.
+require_once __DIR__ . '/fixtures/global-functions.php';
+
 Installation_Manager::instance()->after( function() {
 	$_SERVER['__mantle_after_wordpress_install'] ??= 0;
 
@@ -15,6 +18,10 @@ class Test_Testkit_Install_WordPress extends Test_Case {
 	use Installs_WordPress;
 
 	public function test_mantle_is_installed_from_trait() {
+		$called = \mantle_after_wordpress_install( false );
+
+		$this->assertEquals( 1, $called );
+
 		$this->assertEquals( 1, $_SERVER['__mantle_after_wordpress_install'] ?? 0 );
 	}
 }
