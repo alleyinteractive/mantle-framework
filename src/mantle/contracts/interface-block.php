fde22ee7cc144be5cc65ea2138f5e245418cf489
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-07-28 09:59:27 -0400

    Add a block contract

---
+++ src/mantle/contracts/interface-block.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Block interface file.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Contracts;
+
+/**
+ * Block Contract
+ */
+interface Block {
+	/**
+	 * Render method.
+	 */
+	public function render();
+}
