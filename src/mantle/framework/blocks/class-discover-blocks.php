71ea8f68dc117529b8a5c6dfb5a9ef0922a5f851
Travis Weston <anubisthejackle@users.noreply.github.com>
2022-07-28 09:59:11 -0400

    Add a block discovery class

---
+++ src/mantle/framework/blocks/class-discover-blocks.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Discover_Blocks class file
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Framework\Blocks;
+
+use Mantle\Contracts\Block;
+use Mantle\Support\Str;
+use ReflectionClass;
+use ReflectionException;
+use SplFileInfo;
+use Symfony\Component\Finder\Finder;
+
+use function Mantle\Support\Helpers\collect;
+
+/**
+ * Discover blocks within a specific directory.
+ */
+class Discover_Blocks {
+	/**
+	 * Get all of the blocks by searching the given block directory.
+	 *
+	 * @param string $path Block path.
+	 * @param string $base_path Base path of the application.
+	 * @return array
+	 */
+	public static function within( string $path, string $base_path ): array {
+		$blocks = collect(
+			static::get_blocks(
+				( new Finder() )->files()->in( $path )->name( '*.php' ),
+				$base_path,
+			),
+		);
+
+		return $blocks->all();
+	}
+
+	/**
+	 * Get all of the listeners and their corresponding events.
+	 *
+	 * @param iterable $blocks Listener files.
+	 * @param string   $base_path Base path.
+	 * @return array
+	 */
+	protected static function get_blocks( $blocks, string $base_path ): array {
+
+		$found_blocks = [];
+
+		foreach ( $blocks as $block ) {
+			try {
+				$block = new ReflectionClass(
+					static::class_from_file( $block, $base_path ),
+				);
+			} catch ( ReflectionException $e ) {
+				continue;
+			}
+
+			if ( ! $block->isInstantiable() ) {
+				continue;
+			}
+
+			if( ! $block->implementsInterface( Block::class ) ) {
+				continue;
+			}
+
+			$found_blocks[] = $block->getName();
+		}
+
+		return array_filter( $found_blocks );
+	}
+
+	/**
+	 * Extract the class name from a given file path.
+	 *
+	 * @param SplFileInfo $file File.
+	 * @param string      $base_path Base path.
+	 * @return string
+	 */
+	protected static function class_from_file( SplFileInfo $file, string $base_path ): string {
+		$class = trim(
+			Str::studly_underscore(
+				Str::replace_last( 'class-', '', Str::replace_first( $base_path, '', $file->getRealPath() ) )
+			),
+			DIRECTORY_SEPARATOR,
+		);
+
+		$classname = str_replace(
+			[
+				DIRECTORY_SEPARATOR,
+				ucfirst( basename( app()->get_app_path() ) ) . '\\',
+			],
+			[
+				'\\',
+				app()->get_namespace() . '\\',
+			],
+			ucfirst( Str::replace_last( '.php', '', $class ) ),
+		);
+
+		return $classname;
+	}
+}
---
+++ tests/framework/blocks/fixtures/class-example-block.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * An Example block class
+ * @package Mantle
+ * @subpackage Tests
+ */
+
+namespace Mantle\Tests\Framework\Blocks\Fixtures;
+
+use Mantle\Contracts\Block;
+
+class Example_Block implements Block {
+	public function render() {
+
+	}
+}
