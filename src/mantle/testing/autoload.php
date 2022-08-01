42b2ea93b1b4e60fb4d6d3a243f00e17f864ce73
Sean Fisher <srtfisher@gmail.com>
2022-08-01 19:16:38 -0400

    Include preload always

--- src/mantle/testing/autoload.php
+++ src/mantle/testing/autoload.php
@@ -11,6 +11,8 @@ namespace Mantle\Testing;
 
 use function Mantle\Support\Helpers\tap;
 
+require_once __DIR__ . '/preload.php';
+
 /**
  * Retrieve an instance of the Installation Manager
  *
@@ -21,8 +23,6 @@ use function Mantle\Support\Helpers\tap;
  * @return Installation_Manager
  */
 function manager(): Installation_Manager {
-	require_once __DIR__ . '/preload.php';
-
 	return Installation_Manager::instance();
 }
 
