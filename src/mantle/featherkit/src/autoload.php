a92b75c3ee42ec7e69cccd103ebcffd18f2a6139
Sean Fisher <srtfisher@gmail.com>
2022-12-27 14:53:13 -0500

    Stubbing out featherkit

---
+++ src/mantle/featherkit/src/autoload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Mantle Featherkit Loader
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Featherkit;
+
+/**
+ * Enable Featherkit to define a lightweight Mantle Container
+ */
+function load(
+	array $config = [],
+) {
+	$container
+}
