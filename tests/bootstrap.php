889c34ee8b658babdec774a501e8ddd8bb5be6b0
Sean Fisher <srtfisher@gmail.com>
2023-05-30 13:13:20 -0400

    Lock to 85b3aff18f181fd2c7a08dfd639fa4688fe1179a to debug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,11 @@ define( 'MANTLE_PHPUNIT_FIXTURES_PATH', __DIR__ . '/fixtures' );
 define( 'MANTLE_PHPUNIT_TEMPLATE_PATH', __DIR__ . '/template-parts' );
 define( 'MANTLE_TESTING_DEBUG', true );
 
+// Testing an specific version of mantle-ci.
+putenv( 'MANTLE_CI_BRANCH=85b3aff18f181fd2c7a08dfd639fa4688fe1179a' );
+
 \Mantle\Testing\manager()
 	->maybe_rsync_plugin()
+	->with_object_cache()
 	->with_vip_mu_plugins()
 	->install();
