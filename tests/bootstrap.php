6e9a5bdf31273ed47e1bb357ffe8ca0b652c6d39
Sean Fisher <srtfisher@gmail.com>
2023-05-30 12:27:55 -0400

    Test an old version of mantle ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,9 @@ define( 'MANTLE_PHPUNIT_FIXTURES_PATH', __DIR__ . '/fixtures' );
 define( 'MANTLE_PHPUNIT_TEMPLATE_PATH', __DIR__ . '/template-parts' );
 define( 'MANTLE_TESTING_DEBUG', true );
 
+// Testing an old version of mantle-ci.
+putenv( 'MANTLE_CI_BRANCH=93e2f541a668c4d17eb3b7997c07634ecbd99240' );
+
 \Mantle\Testing\manager()
 	->maybe_rsync_plugin()
 	->with_object_cache()
