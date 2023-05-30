3e9931ad55dbd031297bbf9df4e63876f9b6d656
Sean Fisher <srtfisher@gmail.com>
2023-05-30 14:29:28 -0400

    Disable the branch lock in

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,9 +12,6 @@ define( 'MANTLE_PHPUNIT_FIXTURES_PATH', __DIR__ . '/fixtures' );
 define( 'MANTLE_PHPUNIT_TEMPLATE_PATH', __DIR__ . '/template-parts' );
 define( 'MANTLE_TESTING_DEBUG', true );
 
-// Testing an specific version of mantle-ci.
-putenv( 'MANTLE_CI_BRANCH=revert-9-revert-8-vip-mu-plugins' );
-
 \Mantle\Testing\manager()
 	->maybe_rsync_plugin()
 	->with_vip_mu_plugins()
