b642d8e06339c61b1ea673d831d3b630ec7bfd21
Sean Fisher <srtfisher@gmail.com>
2023-09-13 11:40:42 -0400

    Ensure that attribute and action methods are deduplicated (#447)
    * Ensure that attribute and action methods are deduplicated
    
    * Remove a legacy attribute version check
    
    * Add light tests for typehinting events to ease my mind

--- src/mantle/support/attributes/class-action.php
+++ src/mantle/support/attributes/class-action.php
@@ -12,15 +12,15 @@ use Attribute;
 /**
  * Hook Action Attribute
  *
- * Used to hook a method to an WordPress action at a specific priority.
+ * Used to hook a method to an WordPress hook at a specific priority.
  */
 #[Attribute]
 class Action {
 	/**
 	 * Constructor.
 	 *
-	 * @param string $action Action name.
+	 * @param string $hook_name Hook name.
 	 * @param int    $priority Priority, defaults to 10.
 	 */
-	public function __construct( public string $action, public int $priority = 10 ) {}
+	public function __construct( public string $hook_name, public int $priority = 10 ) {}
 }
