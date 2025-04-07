04790040fc8b56af3c087d9e401c5e34e6834844
Sean Fisher <srtfisher@gmail.com>
2025-04-07 13:31:19 -0400

    Adjust for annotations

--- tests/Database/Factory/FactoryTest.php
+++ tests/Database/Factory/FactoryTest.php
@@ -94,7 +94,7 @@ class FactoryTest extends FrameworkTestCase {
 
 		$this->assertIsArray( $post_ids );
 		$this->assertCount( 3, $post_ids );
-		$this->assertContainsOnly( 'int', $post_ids );
+		$this->assertContainsOnlyInt( $post_ids );
 
 		$posts = Testable_Post::factory()->count( 12 )->create_and_get()->all();
 
