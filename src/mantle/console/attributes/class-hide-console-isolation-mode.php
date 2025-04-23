3ec4ac2ee870066b3b08ff75ae8768f28e02cd69
Sean Fisher <srtfisher@gmail.com>
2025-04-23 14:43:01 -0400

    Fix attribute

--- src/mantle/console/attributes/class-hide-console-isolation-mode.php
+++ src/mantle/console/attributes/class-hide-console-isolation-mode.php
@@ -7,8 +7,10 @@
 
 namespace Mantle\Console\Attributes;
 
+use Attribute;
+
 /**
  * Attribute to automatically hide a command in console isolation mode.
  */
-#[\Attribute(Attribute::TARGET_CLASS)]
+#[Attribute( Attribute::TARGET_CLASS )]
 class Hide_Console_Isolation_Mode {}
