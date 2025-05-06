2d82a8e6e5bf7b66b6a317f2e6aadc128f0e5da7
Sean Fisher <srtfisher@gmail.com>
2025-05-06 16:07:08 -0400

    Improve the theme wrapper to use block templates

---
+++ src/mantle/framework-views/autoload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Framework Views.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Framework_Views;
+
+/**
+ * Path to the framework views directory.
+ */
+function get_framework_views_path(): string {
+	return __DIR__ . '/views/';
+}
