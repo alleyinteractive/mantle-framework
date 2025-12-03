6d778b3961821fbfe2bb28a78452a417bcfc6cba
Sean Fisher <srtfisher@gmail.com>
2025-12-03 12:41:21 -0500

    Adjust lang

--- src/mantle/testing/wordpress-bootstrap.php
+++ src/mantle/testing/wordpress-bootstrap.php
@@ -197,7 +197,7 @@ if ( $multisite && ! $installing_wp ) {
 	defined( 'SUBDOMAIN_INSTALL' ) || define( 'SUBDOMAIN_INSTALL', false );
 	$GLOBALS['base'] = '/';
 } elseif ( ! $installing_wp ) {
-	Utils::info( "Running as single site...\n<br>ℹ️ To run multisite, pass <span class=\"text-orange-500\">WP_MULTISITE=1</span> or set the <span class=\"text-orange-500\">WP_TESTS_MULTISITE=1</span> constant." );
+	Utils::info( "Running as single site...\n<br>ℹ️ To run multisite, call <span class=\"text-orange-500\">with_multisite()</span> on the installation manager." );
 }
 
 unset( $multisite );
@@ -239,7 +239,7 @@ tests_add_filter( 'enable_loading_object_cache_dropin', function ( $enable_objec
 require_once ABSPATH . '/wp-settings.php';
 
 if ( isset( $GLOBALS['wp_version'] ) ) {
-	Utils::info( 'Running WordPress version ' . $GLOBALS['wp_version'] . ( is_multisite() ? ' multisite' : ' single site' ) . '.' );
+	Utils::info( 'Running WordPress version ' . $GLOBALS['wp_version'] . ( is_multisite() ? ' multisite' : ' single site' ) );
 } else {
 	Utils::error( '🚨 WordPress version not detected!' );
 }
