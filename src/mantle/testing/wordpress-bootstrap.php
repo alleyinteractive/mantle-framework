3351ad7297053657df38da100bcea9a538b7aaa0
Sean Fisher <srtfisher@gmail.com>
2025-12-03 12:40:03 -0500

    Drop suffix

--- src/mantle/testing/wordpress-bootstrap.php
+++ src/mantle/testing/wordpress-bootstrap.php
@@ -239,7 +239,7 @@ tests_add_filter( 'enable_loading_object_cache_dropin', function ( $enable_objec
 require_once ABSPATH . '/wp-settings.php';
 
 if ( isset( $GLOBALS['wp_version'] ) ) {
-	Utils::info( 'Running WordPress version ' . $GLOBALS['wp_version'] );
+	Utils::info( 'Running WordPress version ' . $GLOBALS['wp_version'] . ( is_multisite() ? ' multisite' : ' single site' ) . '.' );
 } else {
 	Utils::error( '🚨 WordPress version not detected!' );
 }
