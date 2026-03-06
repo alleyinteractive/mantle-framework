8cb8d1f20702d0ee0776426c3242b5d7920eea85
Matthew Boynes <mboynes+git@alley.co>
2026-03-06 09:28:33 -0500

    Improve admin_screen_tear_down for missing backup case
    Modify tear down to (un)set current screen when no backup exists.

--- src/mantle/testing/concerns/trait-admin-screen.php
+++ src/mantle/testing/concerns/trait-admin-screen.php
@@ -48,10 +48,6 @@ trait Admin_Screen {
 	 * Restore the backed up screen.
 	 */
 	public function admin_screen_tear_down(): void {
-		if ( isset( $this->backup_screen ) ) {
-			set_current_screen( $this->backup_screen );
-		}
-
 		unset(
 			$GLOBALS['pagenow'],
 			$GLOBALS['wp_importers'],
@@ -60,5 +56,11 @@ trait Admin_Screen {
 			$GLOBALS['typenow'],
 			$GLOBALS['taxnow']
 		);
+		
+		if ( isset( $this->backup_screen ) ) {
+			set_current_screen( $this->backup_screen );
+		} else {
+			set_current_screen();
+		}
 	}
 }
